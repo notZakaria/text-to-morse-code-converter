17970dd755e240f75240dd9d2ba31f2a06056e1f
JaZaka <zakaria@here.com>
2022-11-19 16:36:46 +0100

    add morse converter

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # This is a script that converts text to morse code
+
+text = [a for a in "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,?!"]
+morse = [c for c in '.-c-...c-.-.c-..c.c..-.c--.c....c..c.---c-.-c.-..c--c-.c---c.--.c--.-c.-.c...c-c..-c...-c.--c-' \
+                    '..-c-.--c--..c-----c.----c..---c...--c....-c.....c-....c--...c---..c----.c.-.-.-c--..--c..--..c-' \
+                    '.-.--'.split('c')]
+
+morse_dict = {key: value for key, value in zip(text, morse)}
+
+inp = input("Enter your text: ").upper()
+print("Morse code:", end=" ")
+for ch in inp:
+    try:
+        print(morse_dict[ch], end=" ")
+    except KeyError:
+        if ch == " ":
+            print(" ", end=" ")
+        else:
+            print(f'{ch} is not a valid character')
